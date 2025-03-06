32e2437969337793a5bda9272b2c7343c759cacd
profPane <101067625+profPane@users.noreply.github.com>
2025-03-06 06:35:17 +0000

    Fix modulo registrazione amministrativa

--- auth/register.php
+++ auth/register.php
@@ -65,80 +65,79 @@ if (isset($_POST['register'])) { //
         }
     }
     printf($msg, '<br><a href="../index.html">torna indietro</a>');
-} else { //se non arrivo da un post allora visualizzo il form!
+} else { //se non arrivo da un post allora visualizzo il form! Ma solo se autorizzato
     $livello = -1; //-1 == non loggato 
-        if (isset($_SESSION['session_id'])) {
-            $session_user = htmlspecialchars($_SESSION['session_user'], ENT_QUOTES, 'UTF-8');
-            $session_id = htmlspecialchars($_SESSION['session_id']);
-            if (isset($_SESSION['livello'])==TRUE) { //se sono passato per il login
-                $livello = $_SESSION['livello']; //allora trovo livello in $_SESSION e lo prelevo
-            } else {
-                //altrimenti lo imposto a -1 valore che usero per "utente non loggato" 
-                $livello=-1; 
-            }
-            echo "<br>";
-            switch ($livello) {
-                case '-1':
-                    printf("Benvenuto Ospite, puoi registrarti per un account");
-                    break;
-                case '0':
-                    printf("Benvenuto %s, il tuo session ID è %s puoi registrare un altro ospite", $session_user, $session_id);
-                    break;  
-                case '8':
-                    printf("Benvenuto %s, il tuo session ID è %s e hai i poteri di amministrazione", $session_user, $session_id);
-                    break;
-                case '9':
-                    printf("Benvenuto %s, il tuo session ID è %s e hai i poteri della supermucca", $session_user, $session_id);
-                    break;
-                default:
-                    printf("Benvenuto %s, non dovresti essere qui", $session_user, $session_id);
-                    printf("Effettua il %s per accedere all'area riservata", '<a href="login.html">login</a>');
-                    die();
-                    break;
-            }
-            printf("%s", '<br><a href="auth/logout.php">logout</a><br><a href="index.html">homepage</a><br>');
-        } else { //non c'è una sessione attiva
-            printf("Effettua il %s per accedere all'area riservata", '<a href="login.html">login</a>');
+    if (isset($_SESSION['session_id'])) {
+        $session_user = htmlspecialchars($_SESSION['session_user'], ENT_QUOTES, 'UTF-8');
+        $session_id = htmlspecialchars($_SESSION['session_id']);
+        if (isset($_SESSION['livello'])==TRUE) { //se sono passato per il login
+            $livello = $_SESSION['livello']; //allora trovo livello in $_SESSION e lo prelevo
+        } else {
+            //altrimenti lo imposto a -1 valore che usero per "utente non loggato" 
+            $livello=-1; 
         }
-
-        ?>
-
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>Registrazione</title>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap">
-                <link rel="stylesheet" href="/css/style.css">
-            </head>
-            <body>
-                <form method="post" action="/auth/register.php">
-                    <h1>Registrazione</h1>
-                    <input type="text" id="username" placeholder="Username" name="username" maxlength="50" required>
-                    <input type="password" id="password" placeholder="Password" name="password" required>
-                    <?php    
-                    //logica per "stampare" o meno il campo select
-                        if ($livello>7) {  //sono almeno SU stampo la scelta livello 
-                            $scelte='<label for="livello">Livello utente</label>
-                            <select name="livello" id="livello">
-                            <option value=0>Ospite</option>
-                            <option value=1>Utente base</option>
-                            <option value=7>superuser</option>s
-                            <option value=8>admin</option>
-                            <option value=9>root</option>
-                            </select>';
-                        } else { 
-                            $scelte=$scelte='<label for="livello">Livello utente</label>
-                            <select name="livello" id="livello">
-                            <option value=0>Ospite</option>
-                            </select>';;
-                        }
-                        printf("%s",$scelte);
-                    ?>
-                    <button type="submit" name="register">Registrati</button>
-                </form>
-            </body>
-        </html>
-
-<?
+        echo "<br>";
+        switch ($livello) {
+        /*    case '-1':
+                printf("Benvenuto Ospite, puoi registrarti per un account");
+                break;
+            case '0':
+                printf("Benvenuto %s, il tuo session ID è %s puoi registrare un altro ospite", $session_user, $session_id);
+                break; */  
+            case '8':
+                printf("Benvenuto %s, il tuo session ID è %s e hai i poteri di amministrazione", $session_user, $session_id);
+                break;
+            case '9':
+                printf("Benvenuto %s, il tuo session ID è %s e hai i poteri della supermucca", $session_user, $session_id);
+                break;
+            default:
+                printf("Benvenuto %s, non dovresti essere qui", $session_user, $session_id);
+                printf("Effettua il %s per accedere all'area riservata", '<a href="../login.html">login</a>');
+                //die();
+                break;
+        } //non arrivo da POST ma sono autorizzato allora visualizzo il form
+        if ($livello>7) { //sono amministratore? ?>
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Registrazione</title>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap">
+                    <link rel="stylesheet" href="/css/style.css">
+                </head>
+                <body>
+                    <form method="post" action="/auth/register.php">
+                        <h1>Registrazione</h1>
+                        <input type="text" id="username" placeholder="Username" name="username" maxlength="50" required>
+                        <input type="password" id="password" placeholder="Password" name="password" required>
+                        <?php    
+                        //logica per "stampare" o meno il campo select
+                            if ($livello>7) {  //sono almeno SU stampo la scelta livello 
+                                $scelte='<label for="livello">Livello utente</label>
+                                <select name="livello" id="livello">
+                                <option value=0>Ospite</option>
+                                <option value=1>Utente base</option>
+                                <option value=7>superuser</option>s
+                                <option value=8>admin</option>
+                                <option value=9>root</option>
+                                </select>';
+                            } else { 
+                                $scelte=$scelte='<label for="livello">Livello utente</label>
+                                <select name="livello" id="livello">
+                                <option value=0>Ospite</option>
+                                </select>';;
+                            }
+                            printf("%s",$scelte);
+                        ?>
+                        <button type="submit" name="register">Registrati</button>
+                    </form>
+                </body>
+            </html>
+        <? }//fine form           
+        printf("%s", '<br><a href="auth/logout.php">logout</a><br><a href="index.html">homepage</a><br>');
+    } else { //non c'è una sessione attiva
+                printf("Utente non autorizzato, non dovresti essere qui<br>");
+                printf("Effettua il %s per accedere all'area riservata", '<a href="../login.html">login</a>');
+                die();        
+    }
 } // fine else della verifica arrivo da POST o meno
 ?>
